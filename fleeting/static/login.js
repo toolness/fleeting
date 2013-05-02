b8b33e3fb0b8cbb8c2b87513049d5f3797f79235
Atul Varma <varmaa@gmail.com>
2013-05-02 09:15:25 -0400

    migrated to persona observer api.

--- fleeting/static/login.js
+++ fleeting/static/login.js
@@ -1,23 +1,28 @@
-$(window).ready(function() {
+window.reloadPage = function() {
+  // We bind this to a global so we can stub it out in test suites, as
+  // location.reload can't be stubbed.
+  location.reload();
+};
+
+(function() {
   var csrfToken = $('meta[name="csrf"]').attr('content');
+  var email = $('meta[name="email"]').attr('content') || null;
 
-  $(".js-logout").click(function() {
-    jQuery.post("/logout", {
-      _csrf_token: csrfToken
-    }, function(text) {
-      window.location.reload();
-    });
-  });
-  $(".js-login").click(function() {
-    navigator.id.get(function(assertion) {
-      if (!assertion) return;
-
-      jQuery.post("/login", {
+  navigator.id.watch({
+    loggedInUser: email,
+    onlogin: function(assertion) {
+      $.post("/login", {
         assertion: assertion,
         _csrf_token: csrfToken
-      }, function(text) {
-        window.location.reload();
-      });
-    });
+      }, reloadPage);
+    },
+    onlogout: function() {
+      $.post("/logout", {
+        _csrf_token: csrfToken
+      }, reloadPage);      
+    }
   });
-});
+
+  $("body").on("click", ".js-login", function() { navigator.id.request(); });
+  $("body").on("click", ".js-logout", function() { navigator.id.logout(); });
+})();
