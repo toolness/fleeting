b565f706f365a91243523d10166bdf3ac1ca09e7
Atul Varma <varmaa@gmail.com>
2013-05-05 08:55:23 -0400

    moved force_preferred_url_scheme to utils, added tests.

--- fleeting/utils.py
+++ fleeting/utils.py
@@ -1,4 +1,5 @@
 import os
+from urllib import quote
 
 ROOT = os.path.dirname(os.path.abspath(__file__))
 
@@ -8,3 +9,31 @@ def ensure_env_vars(vars):
     for key in vars:
         if not key in os.environ:
             raise KeyError('environment variable %s is not defined' % key)
+
+def force_preferred_url_scheme(app, originating_scheme_header=None):
+    wsgi_app = app.wsgi_app
+    osh = originating_scheme_header
+    if osh:
+        osh = 'HTTP_%s' % osh.replace('-', '_').upper()
+
+    def force_preferred_url_scheme_middleware(environ, start_response):
+        site_scheme = app.config['PREFERRED_URL_SCHEME']
+
+        if osh and environ.get(osh) == 'https':
+            environ['wsgi.url_scheme'] = 'https'
+
+        # If the SITE_URL is explicitly defined, then we're going to
+        # force a redirect to the proper protocol (http or https)
+        # if necessary.
+        if environ['wsgi.url_scheme'] != site_scheme:
+            url = '%s://%s' % (site_scheme, app.config['SERVER_NAME'])
+            # http://www.python.org/dev/peps/pep-0333/#url-reconstruction
+            url += quote(environ.get('SCRIPT_NAME', ''))
+            url += quote(environ.get('PATH_INFO', ''))
+            if environ.get('QUERY_STRING'):
+                url += '?' + environ['QUERY_STRING']
+            start_response("302 Found", [("Location", url)])
+            return ["redirect to %s" % site_scheme]
+        return wsgi_app(environ, start_response)
+
+    app.wsgi_app = force_preferred_url_scheme_middleware
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,8 +1,43 @@
 import unittest
 
+import mock
+
 from fleeting import utils
 
 class UtilsTests(unittest.TestCase):
+    def test_force_preferred_url_scheme_delegates_to_app(self):
+        app = mock.MagicMock()
+        app.config = dict(PREFERRED_URL_SCHEME='https', SERVER_NAME='f.org')
+        start_response = mock.MagicMock()
+        environ = {'HTTP_X_FORWARDED_PROTO': 'https',
+                   'wsgi.url_scheme': 'http'}
+        wsgi_app = app.wsgi_app
+        utils.force_preferred_url_scheme(app, 'X-Forwarded-Proto')
+        res = app.wsgi_app(environ, start_response)
+
+        self.assertEqual(start_response.call_count, 0)
+        self.assertEqual(environ['wsgi.url_scheme'], 'https')
+        wsgi_app.assert_called_once_with(environ, start_response)
+        self.assertTrue(res is wsgi_app.return_value)
+
+    def test_force_preferred_url_scheme_redirects(self):
+        app = mock.MagicMock()
+        app.config = dict(PREFERRED_URL_SCHEME='https', SERVER_NAME='f.org')
+        start_response = mock.MagicMock()
+        environ = dict(SCRIPT_NAME='',
+                       PATH_INFO='/goop',
+                       QUERY_STRING='ack=foob')
+        environ['wsgi.url_scheme'] = 'http'
+        wsgi_app = app.wsgi_app
+        utils.force_preferred_url_scheme(app)
+        res = app.wsgi_app(environ, start_response)
+
+        start_response.assert_called_once_with("302 Found", [
+            ("Location", "https://f.org/goop?ack=foob")
+        ])
+        self.assertEqual(wsgi_app.call_count, 0)
+        self.assertEqual(res, ["redirect to https"])
+
     def test_ensure_env_vars_raises_exception(self):
         msg = 'environment variable QWOP is not defined'
         with self.assertRaisesRegexp(KeyError, msg):
